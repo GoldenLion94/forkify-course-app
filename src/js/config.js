e61fabc3bd7ac594d3789f8a5e9ab77a86acce1e
GoldenLion94 <miltonsanchez5081@gmail.com>
2023-12-25 23:31:24 -0500

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '024b7e68-51c4-45f5-8fb8-930fd565f167';
+export const MODAL_CLOSE_SEC = 2.5;
